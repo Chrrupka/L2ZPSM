169b3fc050f3e6cfbe4180faefb41cddc966b0d8
Edyta Boczarska <92117280+Chrrupka@users.noreply.github.com>
2022-11-16 19:12:41 +0000

    zad 5

--- app.js
+++ app.js
@@ -36,3 +36,31 @@ function findSubject(usr,weightValue){
 }
 
 findSubject(user,1)
+
+
+const collections = [
+    {},
+    15,
+    "hello@test.pl",
+    null,
+    ['aaa', 'bbb', 5],
+    'admin@gmail.com',
+    undefined,
+    'a34@yahoo.com',
+    '321@a',
+    '321.pl'
+    ];
+
+function getMails(coll){
+    let arr = []
+    const reg = /^[a-z\d]+[\w\d.-]*@(?:[a-z\d]+[a-z\d-]+\.){1,5}[a-z]{2,6}$/i;
+    for(const c of coll){
+        if(typeof(c) === 'string' && reg.test(c)){
+            arr.push(c)
+        }
+    }
+    
+    console.log(arr.sort())
+}
+
+getMails(collections)
