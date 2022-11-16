f4f6968823f64f5726a66b5a3a3e5a4d8592d1c2
Edyta Boczarska <92117280+Chrrupka@users.noreply.github.com>
2022-11-16 19:07:09 +0000

    zad 4

--- app.js
+++ app.js
@@ -3,3 +3,36 @@ let arr = [1, 2, 3, 4, 5, 6]
 
 console.log(_.mean( arr ))
 console.log("Max: ", Math.max(...arr),"\n","Min:", Math.min(...arr))
+
+const user = {
+  name: 'Imie',
+  surname: 'Nazwisko',
+  allGrades: [
+    {
+      subjectName: 'Name1',
+      grades: [5,4,3,5,2],
+      weight: 3
+    },
+    {
+      subjectName: 'Name2',
+      grades: [3, 3.5, 2],
+      weight: 1
+    },
+    {
+      subjectName: 'Name3',
+      grades: [4, 3, 3.5],
+      weight: 5
+    }
+  ]
+};
+
+function findSubject(usr,weightValue){
+    let result = _.find(usr.allGrades, function(obj){
+        if(obj.weight === weightValue){
+            return true
+        }
+    })
+    console.log(result.subjectName)
+}
+
+findSubject(user,1)
